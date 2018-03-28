a32efaff1182563fd58c00ffe3f2b30472c2e22c
Timofei Volkov <Timofei_Volkov@epam.com>
2018-03-28 21:45:06 +0200

    collections/P01

--- exercises/src/main/scala/pl/japila/scalania/collection/P01.scala
+++ exercises/src/main/scala/pl/japila/scalania/collection/P01.scala
@@ -1,5 +1,8 @@
 package pl.japila.scalania.collection
 
 object P01 {
-  def filterNot[T](ts: Seq[T], p: T => Boolean): Seq[T] = ???
+  def filterNot[T](ts: Seq[T], p: T => Boolean): Seq[T] = {
+    for (t <- ts if !p(t)) yield t
+  }
+
 }
