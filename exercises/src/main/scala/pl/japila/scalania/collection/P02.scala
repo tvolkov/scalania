1582808911f35942ab829229d5b55f5e90070402
Timofei Volkov <Timofei_Volkov@epam.com>
2018-03-28 22:03:24 +0200

    collections/P02

--- exercises/src/main/scala/pl/japila/scalania/collection/P02.scala
+++ exercises/src/main/scala/pl/japila/scalania/collection/P02.scala
@@ -1,5 +1,7 @@
 package pl.japila.scalania.collection
 
 object P02 {
-  def checkNegatives[T](m: Map[T, Seq[Int]]): Map[T, Boolean] = ???
+  def checkNegatives[T](m: Map[T, Seq[Int]]): Map[T, Boolean] = {
+    m map {case (k, v) => (k, v.forall(x => x < 0))}
+  }
 }
